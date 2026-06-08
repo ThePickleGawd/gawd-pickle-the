0833e7260040c51f0edea2cc414ee69a0f385122
Dylan Lu <dylanelu@gmail.com>
2026-06-08 12:39:15 -0700

    fix tokenizer

--- utils/train_tokenizer.py
+++ utils/train_tokenizer.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import time
 
 from gpt.tokenizer.bpe import Tokenizer, train_bpe
@@ -15,6 +16,7 @@ vocab, merges = train_bpe(input_path, 10000, special_tokens)
 tokenizer = Tokenizer(vocab, merges, special_tokens)
 
 print(f"Exporting to:\n vocab: {vocab_path}\n merges: {merges_path}")
+Path(vocab_path).parent.mkdir(parents=True, exist_ok=True)
 tokenizer.export(vocab_path, merges_path)
 
 print(f"Finished in {time.time() - t0}s")
