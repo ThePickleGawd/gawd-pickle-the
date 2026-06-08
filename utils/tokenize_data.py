a0c0fb7535ce7e6a26691cb6f1664ee8a687a031
Dylan Lu <dylanelu@gmail.com>
2026-06-08 11:41:47 -0700

    use tinystories for now

--- utils/tokenize_data.py
+++ utils/tokenize_data.py
@@ -1,28 +1,29 @@
+import time
+
 import numpy as np
+from tqdm import tqdm
 
 from gpt.tokenizer.bpe import Tokenizer
 
-vocab_path = "checkpoints/tokenizer/vocab.txt"
-merges_path = "checkpoints/tokenizer/merges.txt"
+vocab_path = "checkpoints/tokenizer/test/vocab.txt"
+merges_path = "checkpoints/tokenizer/test/merges.txt"
 special_tokens = ["<|endoftext|>"]
 
-input_data_path = "data/TinyStoriesV2-GPT4-train.txt"
-input_val_path = "data/TinyStoriesV2-GPT4-valid.txt"
-train_out_path = "data/train.npy"
-val_out_path = "data/val.npy"
+input_data_path = "tests/fixtures/tinystories_sample_5M.txt"
+input_val_path = "tests/fixtures/tinystories_sample.txt"
+train_out_path = "data/tinystories_train.npy"
+val_out_path = "data/tinystories_val.npy"
+
+t0 = time.time()
 
 tokenizer = Tokenizer.from_files(vocab_path, merges_path, special_tokens)
 
-# Load data
+# Encode data
 with open(input_data_path, "r") as f:
-    train_data = f.read()
+    train_ids = list(tqdm(tokenizer.encode_iterable(f), desc="train", unit="tok"))
 
 with open(input_val_path, "r") as f:
-    val_data = f.read()
-
-# Encode data
-train_ids = tokenizer.encode(train_data)
-val_ids = tokenizer.encode(val_data)
+    val_ids = list(tqdm(tokenizer.encode_iterable(f), desc="val", unit="tok"))
 
 print(
     f"Train has {len(train_ids)} tokens",
@@ -34,3 +35,5 @@ train_ids = np.array(train_ids, dtype=np.uint16)
 val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(train_out_path)
 val_ids.tofile(val_out_path)
+
+print(f"Finished in {time.time() - t0}s")
--- utils/train_tokenizer.py
+++ utils/train_tokenizer.py
@@ -1,14 +1,20 @@
+import time
+
 from gpt.tokenizer.bpe import Tokenizer, train_bpe
 
-vocab_path = "checkpoints/tokenizer/vocab.txt"
-merges_path = "checkpoints/tokenizer/merges.txt"
+vocab_path = "checkpoints/tokenizer/tinystories/vocab.txt"
+merges_path = "checkpoints/tokenizer/tinystories/merges.txt"
 
-input_path = "tests/fixtures/tinystories_sample_5M.txt"
+input_path = "data/TinyStoriesV2-GPT4-train.txt"
 
 special_tokens = ["<|endoftext|>"]
 
-vocab, merges = train_bpe(input_path, 1024, special_tokens)
+t0 = time.time()
+
+vocab, merges = train_bpe(input_path, 10000, special_tokens)
 tokenizer = Tokenizer(vocab, merges, special_tokens)
 
 print(f"Exporting to:\n vocab: {vocab_path}\n merges: {merges_path}")
 tokenizer.export(vocab_path, merges_path)
+
+print(f"Finished in {time.time() - t0}s")
